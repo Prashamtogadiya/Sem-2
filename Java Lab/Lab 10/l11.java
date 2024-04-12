8fe51507c531fe797222262122dfc6aaab3dd79e
Prashamtogadiya <pktogadiya222@gmail.com>
2024-04-12 08:37:35 +0530

    Lab 10 B,C Added

---
+++ Java Lab/Lab 10/l11.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class l11 {
+	public static void main(String[] args) {
+		try {
+			String filename = args[0];
+			File f = new File(filename);
+			FileReader reader = new FileReader(filename);
+			int count = 0;
+			int temp = reader.read();
+			while (temp != -1) {
+				char c = (char) temp;
+				temp = reader.read();
+				if (c == '5') {
+					count++;
+				}
+			}
+			System.out.println("Digit 5 is repeated " + count + " times");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
